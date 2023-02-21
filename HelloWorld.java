ad2124f89adc84659b16b64ae011836bd267f070
Adarsh <adarshpatel2412@gmail.com>
2023-02-21 08:44:11 +0530

    commiting jenkins file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world through cmd maven Ethan");
+		System.out.println("Hello world through cmd maven Maveric");
 	}
 
 }
