538a74e1452468aa67bf153230ddc028dd997223
Adarsh <adarshpatel2412@gmail.com>
2023-02-22 07:39:45 +0530

    adding .class file

---
+++ JobChain_01.java
@@ -0,0 +1,8 @@
+public class JobChain_01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("It's not over till it's not over");
+	}
+
+}
---
+++ JobChain_02 .java
@@ -0,0 +1,8 @@
+public class JobChain_02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("It's not over Utill it's not over");
+	}
+
+}
