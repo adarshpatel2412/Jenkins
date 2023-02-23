0e25ebbb5a716702a4d6700fbd0dcb6534f23ba2
Adarsh <adarshpatel2412@gmail.com>
2023-02-23 07:28:26 +0530

    adding .text file

--- JobChain_02.java
+++ JobChain_02.java
@@ -1,8 +1,8 @@
-public class JobChain_02 {
+public class  {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("It's not over utill it's not over");
+		System.out.println("Hello world through cmd maven Maveric");
 	}
 
 }
---
+++ Last_job07.java
@@ -0,0 +1,8 @@
+public class Last_job07  {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is my last");
+	}
+
+}
